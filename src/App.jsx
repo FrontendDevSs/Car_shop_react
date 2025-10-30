efe09bd60ef94b4ec8d3453a3fcfcbbf6e3ebc6b
Sava Dumancic <sdumancic@bettercollective.local>
2025-10-30 11:45:09 +0100

    Feat: footer and routing logic added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import Navbar from "./components/layout/Navbar/Navbar"
+import Home from "./pages/Home/Home"
+import Shop from "./pages/Shop";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import Footer from "./components/layout/Footer/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-        <>
+        <BrowserRouter>
             <Navbar />
-        </>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/shop" element={<Shop />}></Route>
+                <Route path="/about" element={<About />}></Route>
+                <Route path="/contact" element={<Contact />}></Route>
+                <Route path="/cart" element={<Cart />}></Route>
+            </Routes>
+            <Footer />
+        </BrowserRouter>
     )
 }
 
---
+++ src/components/layout/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import "./_footer.scss";
+
+function Footer() {
+    return(
+        <footer className="footer">
+            <p>© 2024 exemple.com. All rights reserved.</p>
+            <ul>
+                <li>Tearms & Conditions</li>
+                <span>•</span>
+                <li>Privacy Notice</li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
