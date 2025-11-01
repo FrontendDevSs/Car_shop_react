3d3a7d8e94f4d463e2ffd03916345fbdcaa91541
Luka B <lukab95.dev@gmail.com>
2025-11-01 17:53:09 +0100

    Feat: add TopCars section, Card component and axios fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import Button from "./components/ui/button/Button"
-import Home from "./pages/Home/Home"
-
+import Button from "./components/ui/button/Button";
+import Home from "./pages/Home/Home";
+import useFetch from "./hooks/useFetch";
+import { BACKEND_URL } from "./config";
+import Card from "./components/ui/card/Card.jsx";
+import TopCars from "./pages/Home/TopCars.jsx";
 
 function App() {
+  const { data: cars, loading, error } = useFetch(BACKEND_URL);
+  console.log(cars);
+  console.log(error);
+  console.log(loading);
 
-
-    return (
-    <>
-    <h1>Hello there!</h1>
-    <Home />
-    
-    
-    </>
-    )
+  return <></>;
 }
 
-export default App
+export default App;
